65153f41fb865e6eeca9109bb80bc36a55886908
Tanmay Kumar <tan2006may@gmail.com>
2026-05-21 15:01:18 +0530

    fixed openlane pdn pitch crash by switching to absolute floorplan sizing

--- OpenLane/config.tcl
+++ OpenLane/config.tcl
@@ -9,8 +9,14 @@ set ::env(VERILOG_FILES) [concat \
 set ::env(CLOCK_PORT) clk
 set ::env(CLOCK_PERIOD) 10.0
 
-set ::env(FP_CORE_UTIL) 30
-set ::env(PL_TARGET_DENSITY) 0.55
+set ::env(FP_SIZING) "absolute"
+set ::env(DIE_AREA) "0 0 150 150"
+set ::env(CORE_AREA) "10 10 140 140"
+set ::env(PL_TARGET_DENSITY) 0.40
+set ::env(FP_PDN_AUTO_ADJUST) 0
+
+set ::env(FP_PDN_VPITCH) 8
+set ::env(FP_PDN_HPITCH) 8
 set ::env(STD_CELL_LIBRARY) sky130_fd_sc_hd
 
 set ::env(RUN_LINTER) 0
